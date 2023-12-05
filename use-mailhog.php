a54bf87fc22555a0c835226d7ba30103ce3f6376
Corey Salzano <938411+csalzano@users.noreply.github.com>
2023-12-05 10:37:00 -0500

    Adds the plugin file

---
+++ use-mailhog.php
@@ -0,0 +1,42 @@
+<?php
+/**
+ * Plugin Name:       Use MailHog
+ * Description:       Configure WordPress on Valet to use MailHog
+ * Version:           1.0.0
+ * Tags:              laravel valet, mailhog, local, email
+ *
+ * @since             1.0.0
+ * @package           use-mailhog
+ * @author            Daniel Bishop <bishless@gmail.com>
+ * @author            Corey Salzano <938411+csalzano@users.noreply.github.com>
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+if ( ! function_exists( 'phpmailer_use_mailhog' ) ) {
+	add_action( 'phpmailer_init', 'phpmailer_use_mailhog', 10, 1 );
+	/**
+	 * Callback function on phpmailer_init. Initializes phpmailer to use MailHog
+	 * while running WordPress in Laravel Valet.
+	 *
+	 * @param  PHPMailer $phpmailer The PHPMailer instance (passed by reference).
+	 * @return void
+	 */
+	function phpmailer_use_mailhog( $phpmailer ) {
+		// Define that we are sending with SMTP.
+		$phpmailer->isSMTP();
+
+		// The hostname of the mailserver.
+		// phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+		$phpmailer->Host = 'localhost';
+
+		// Use SMTP authentication.
+		// phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+		$phpmailer->SMTPAuth = false;
+
+		// SMTP port number.
+		// Mailhog normally run on port 1025.
+		// phpcs:ignore WordPress.NamingConventions.ValidVariableName.UsedPropertyNotSnakeCase
+		$phpmailer->Port = defined( 'WP_DEBUG' ) && WP_DEBUG ? '1025' : '25';
+	}
+}
